82e618fef8b09edd5557c5a02c006895719f82e3
Shaheer-rossoneri14 <hshaheer99@gmail.com>
2022-05-23 23:27:42 +0530

    V1.0

--- faq/models.py
+++ faq/models.py
@@ -10,6 +10,7 @@ topic_choices = [
 
 # Create your models here.
 class FAQ(models.Model):
+    auto_increment_id = models.AutoField(primary_key=True)
     topic = models.CharField(max_length = 100, choices = topic_choices, default="Others")
     question = models.TextField()
     answer = models.TextField()
