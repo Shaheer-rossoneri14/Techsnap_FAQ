ac1b715c32f3d274b4d18c32381ff8d743f5f5fe
Shaheer-rossoneri14 <hshaheer99@gmail.com>
2022-05-23 18:27:58 +0530

    Added files and changes required for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn techsnap_faq.wsgi --log-file -
+
--- techsnap_faq/settings.py
+++ techsnap_faq/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIRS = (
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-+7_$fc2f(!vxf_%#w-=azhflq!vgt2sz8&1_0bb6ld_!rv&&lz'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-+7_$fc2f(!vxf_%#w-=azhflq!vgt2sz8&1_0bb6ld_!rv&&lz'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-+7_$fc2f(!vxf_%#w-=azhflq!vgt2sz8&1_0bb6ld_!rv&&lz')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,13 +117,23 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),   
+]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
