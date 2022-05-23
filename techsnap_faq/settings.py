e8427b667880e1ef36c9a451e5443081d66e02ac
Shaheer-rossoneri14 <hshaheer99@gmail.com>
2022-05-23 20:44:27 +0530

    V1.0

--- techsnap_faq/settings.py
+++ techsnap_faq/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-+7_$fc2f(!vxf_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://techsnap-faq.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['techsnap-faq.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
